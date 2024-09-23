e343b7f3acbdb437ef9b633bc54ef7c6cfbea697
Caleb Adepitan <calebpitan@gmail.com>
2024-09-24 00:32:46 +0100

    update: add stack components

---
+++ src/components/stack/HStack.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface HStackProps {
+  spacing?: number
+}
+
+withDefaults(defineProps<HStackProps>(), { spacing: 8 })
+const factor = 1 / 2
+</script>
+
+<template>
+  <div class="s-hstack" :style="{ gap: `${spacing * factor}rem` }">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.s-hstack {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+</style>
---
+++ src/components/stack/VStack.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface VStackProps {
+  spacing?: number
+}
+
+withDefaults(defineProps<VStackProps>(), { spacing: 8 })
+const factor = 1 / 2
+</script>
+
+<template>
+  <div class="s-vstack" :style="{ gap: `${spacing * factor}rem` }">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.s-vstack {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+}
+</style>
