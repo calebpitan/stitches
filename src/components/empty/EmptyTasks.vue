363a89f360b7020c3020fe6b0cb89951902e0593
Caleb Adepitan <calebpitan@gmail.com>
2024-09-24 13:46:59 +0100

    feat: empty task state component

---
+++ src/components/empty/EmptyTasks.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import VStack from '../stack/VStack.vue'
+</script>
+
+<template>
+  <VStack class="s-empty" :spacing="4">
+    <span class="pi pi-list s-empty-illustration" />
+
+    <div class="s-empty-message">
+      <slot name="message">
+        <span>What would you like to do today 🤔? </span>
+        <span>Don't overthink it. Create a task now and get kicking!</span>
+      </slot>
+    </div>
+  </VStack>
+</template>
+
+<style scoped>
+.s-empty {
+  user-select: none;
+  align-items: center;
+  max-width: 350px;
+  margin-inline: auto;
+}
+
+.s-empty-illustration {
+  font-size: 1.5rem;
+  background-color: var(--p-primary-color);
+  color: var(--s-surface-middle);
+  border-radius: 50%;
+  padding: 0.75rem;
+  position: relative;
+
+  &::after {
+    content: '💭';
+    position: absolute;
+    top: -0.4rem;
+  }
+}
+
+.s-empty-message {
+  text-align: center;
+  font-size: 0.875rem;
+  color: var(--s-script-secondary);
+}
+</style>
