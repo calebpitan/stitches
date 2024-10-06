7105959b3510797e0610aa19919a62b061579ee8
Caleb Adepitan <calebpitan@gmail.com>
2024-10-06 13:51:57 +0100

    update: rename and add icons

---
+++ src/components/icons/IconClearAll.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { SVGAttributes } from 'vue'
+
+import ClearAll from '@/assets/icons/clear-all.svg'
+
+import SIcon from './SIcon.vue'
+
+interface Props extends /* @vue-ignore */ SVGAttributes {}
+
+defineOptions({ inheritAttrs: false })
+defineProps<Props>()
+</script>
+
+<template>
+  <SIcon>
+    <ClearAll width="1em" height="1em" fill="currentColor" v-bind="$attrs" />
+  </SIcon>
+</template>
---
+++ src/components/icons/IconSidebar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { SVGAttributes } from 'vue'
+
+import Sidebar from '@/assets/icons/sidebar.svg'
+
+import SIcon from './SIcon.vue'
+
+interface Props extends /* @vue-ignore */ SVGAttributes {}
+
+defineOptions({ inheritAttrs: false })
+defineProps<Props>()
+</script>
+
+<template>
+  <SIcon>
+    <Sidebar width="1em" height="1em" fill="currentColor" v-bind="$attrs" />
+  </SIcon>
+</template>
