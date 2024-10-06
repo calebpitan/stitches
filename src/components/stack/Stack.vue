5578a6bf640273c222ab89a5d22b8791289eaa2b
Caleb Adepitan <calebpitan@gmail.com>
2024-10-06 14:26:29 +0100

    update: add stack component

---
+++ src/components/stack/Stack.vue
@@ -0,0 +1,21 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import HStack from './HStack.vue'
+import VStack from './VStack.vue'
+
+interface StackProps {
+  type: 'hstack' | 'vstack'
+  spacing?: number
+}
+
+withDefaults(defineProps<StackProps>(), { spacing: 8 })
+</script>
+
+<template>
+  <HStack v-if="type === 'hstack'">
+    <slot />
+  </HStack>
+  <VStack v-else>
+    <slot />
+  </VStack>
+</template>
