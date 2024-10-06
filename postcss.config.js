b4da8d083f44162d06077b9337a5ea79a74ecd7f
Caleb Adepitan <calebpitan@gmail.com>
2024-10-06 13:59:52 +0100

    chore(build): update postcss config with global data plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,20 @@
+import postcssGlobalData from '@csstools/postcss-global-data'
+
 import postcssPresetEnv from 'postcss-preset-env'
 
 const yourConfig = {
   plugins: [
     /* other plugins */
+    postcssGlobalData({
+      files: ['./src/assets/root.css']
+    }),
     /* remove autoprefixer if you had it here, it's part of postcss-preset-env */
     postcssPresetEnv({
       /* pluginOptions */
       features: {
         'light-dark-function': true,
-        'relative-color-syntax': true
+        'relative-color-syntax': { preserve: true },
+        'custom-media-queries': { preserve: false }
       }
     })
   ]
