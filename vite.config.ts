90c115e8ba0e31d24b6baafd9f67f0400217b00c
Caleb Adepitan <calebpitan@gmail.com>
2024-10-20 10:27:11 +0100

    chore(build): update config and build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import wasm from 'vite-plugin-wasm'
 import svgLoader from 'vite-svg-loader'
@@ -14,9 +15,15 @@ import svgoConfig from './svgo.config.mjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    supported: {
-      'top-level-await': true
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
     }
   },
   plugins: [
@@ -24,9 +31,15 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
     wasm(),
+    topLevelAwait(),
     Components({ resolvers: [PrimeVueResolver()] }),
     svgLoader({ defaultImport: 'component', svgoConfig: svgoConfig as any })
   ],
+  worker: {
+    plugins() {
+      return [wasm(), topLevelAwait()]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
