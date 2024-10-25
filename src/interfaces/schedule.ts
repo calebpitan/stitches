455a242b25eb01f34cd1c358824dc79183e958e4
Caleb Adepitan <calebpitan@gmail.com>
2024-10-25 18:32:23 +0100

    update: types and serializers

--- src/interfaces/schedule.ts
+++ src/interfaces/schedule.ts
@@ -1,24 +1,75 @@
-import type { FrequencyType } from '@/utils/scheduling'
+import type { FrequencyType, Ordinals, WeekdayVariable } from '@/utils/scheduling'
 
-export type Frequency = RegularFrequency | CustomFrequency
+export type Frequency = RegularFrequency | CustomFrequency | { type: 'never' }
+
+export interface BaseRegularExpr {
+  every: number
+}
+
+export interface HourlyExpr extends BaseRegularExpr {}
+export interface DailyExpr extends BaseRegularExpr {}
+
+export interface WeeklyExpr extends DailyExpr {
+  subexpr: { weekdays: Array<number> }
+}
+
+export interface MonthlyOnDays {
+  type: 'ondays'
+  days: Array<number>
+}
+
+export interface MonthlyOnThe {
+  type: 'onthe'
+  ordinal: Ordinals
+  weekday: number
+}
+
+export interface MonthlyExpr extends BaseRegularExpr {
+  subexpr: MonthlyOnDays | MonthlyOnThe
+}
+
+export interface YearlyIn {
+  months: Array<number>
+}
+
+export interface YearlyOnThe {
+  ordinal: Ordinals
+  weekday?: number
+  custom?: WeekdayVariable
+}
+
+export interface YearlyExpr extends BaseRegularExpr {
+  subexpr: {
+    in: YearlyIn
+    on?: YearlyOnThe
+  }
+}
+
+export type RegularExpression<
+  T extends Exclude<FrequencyType, 'custom' | 'never'>,
+  E extends BaseRegularExpr
+> = { type: T; expr: E } & BaseFrequency
+
+export interface CronSchedule {
+  expression: string
+  frequency: Exclude<FrequencyType, 'custom'>
+}
 
 export interface BaseFrequency {
   type: FrequencyType
   until: Date | null
 }
 
-export interface RegularFrequency extends BaseFrequency {
-  type: Exclude<FrequencyType, 'custom'>
-}
+export type RegularFrequency =
+  | RegularExpression<'hour', HourlyExpr>
+  | RegularExpression<'day', DailyExpr>
+  | RegularExpression<'week', WeeklyExpr>
+  | RegularExpression<'month', MonthlyExpr>
+  | RegularExpression<'year', YearlyExpr>
 
 export interface CustomFrequency extends BaseFrequency {
   type: Extract<FrequencyType, 'custom'>
   crons: CronSchedule[]
-}
-
-export interface CronSchedule {
-  expression: string
-  frequency: Exclude<FrequencyType, 'custom' | 'never'>
 }
 
 export interface BaseTaskSchedule {
--- src/serializers/schedule.ts
+++ src/serializers/schedule.ts
@@ -1,30 +1,165 @@
-import { Type } from 'class-transformer'
+import { Exclude, Expose, Type } from 'class-transformer'
 
 import type {
+  BaseRegularExpr,
   CronSchedule,
   CustomFrequency,
+  DailyExpr,
   Frequency,
-  RegularFrequency,
-  TaskSchedule
+  HourlyExpr,
+  MonthlyExpr,
+  MonthlyOnDays,
+  MonthlyOnThe,
+  TaskSchedule,
+  WeeklyExpr,
+  YearlyExpr,
+  YearlyIn,
+  YearlyOnThe
 } from '@/interfaces/schedule'
-import type { FrequencyType } from '@/utils/scheduling'
+import { never } from '@/utils'
+import type { FrequencyType, Ordinals, WeekdayVariable } from '@/utils/scheduling'
 
+// ***************************************************************
+// Base Expression Serializer for
+// ***************************************************************
+@Expose()
+abstract class AbstractExprSerializer implements BaseRegularExpr {
+  every: number
+}
+
+// ***************************************************************
+// Hourly and Daily Scheduling Serializers
+// ***************************************************************
+@Expose()
+class HourlyExprSerializer extends AbstractExprSerializer implements HourlyExpr {}
+@Expose()
+class DailyExprSerializer extends AbstractExprSerializer implements DailyExpr {}
+
+// ***************************************************************
+// Weekly Scheduling Serializers
+// ***************************************************************
+@Expose()
+class WeeklySubExprSerializer {
+  @Type(() => Number)
+  weekdays: Array<number>
+}
+
+@Expose()
+class WeeklyExprSerializer extends AbstractExprSerializer implements WeeklyExpr {
+  subexpr: WeeklySubExprSerializer
+}
+
+// ***************************************************************
+// Monthly Scheduling Serializers
+// ***************************************************************
+@Expose()
+class MonthlyOnDaysSerializer implements MonthlyOnDays {
+  @Type(() => String) type: 'ondays'
+  @Type(() => Number) days: number[]
+}
+
+@Expose()
+class MonthlyOnTheSerializer implements MonthlyOnThe {
+  @Type(() => String) type: 'onthe'
+  @Type(() => String) ordinal: Ordinals
+  weekday: number
+}
+
+@Expose()
+class MonthlyExprSerializer extends AbstractExprSerializer implements MonthlyExpr {
+  @Type((o) => {
+    return { onthe: MonthlyOnTheSerializer, ondays: MonthlyOnDaysSerializer }[
+      o!.object.subexpr.type as 'onthe' | 'ondays'
+    ]
+  })
+  subexpr: MonthlyOnDays | MonthlyOnThe
+}
+
+// ***************************************************************
+// Yearly Scheduling Serializers
+// ***************************************************************
+@Expose()
+class YearlyInSerializer implements YearlyIn {
+  @Type(() => Number) months: number[]
+}
+
+@Expose()
+class YearlyOnTheSerializer implements YearlyOnThe {
+  @Type(() => String) custom?: WeekdayVariable | undefined
+  @Type(() => String) ordinal: Ordinals
+  @Type(() => Number) weekday?: number | undefined
+}
+
+@Expose()
+class YearlySubExprSerializer {
+  in: YearlyInSerializer
+  on?: YearlyOnTheSerializer
+}
+
+@Expose()
+class YearlyExprSerializer extends AbstractExprSerializer implements YearlyExpr {
+  subexpr: YearlySubExprSerializer
+}
+
+// ***************************************************************
+// Cron Scheduling Serializer
+// ***************************************************************
+@Expose()
 export class CronScheduleSerializer implements CronSchedule {
   expression: string
-  frequency: Exclude<FrequencyType, 'custom' | 'never'>
+  @Type(() => String) frequency: Exclude<FrequencyType, 'custom' | 'never'>
 }
 
-export class RegularFrequencySerializer implements RegularFrequency {
-  type: Exclude<FrequencyType, 'custom'>
+// ***************************************************************
+// Regular Repeat Frequency Serializer
+// ***************************************************************
+@Expose()
+export class RegularFrequencySerializer {
+  @Exclude() crons: any = undefined
+  @Type(() => String) type: Exclude<FrequencyType, 'custom'>
   @Type(() => Date) until: Date | null
+
+  @Type((o) => {
+    const type = o!.object.type as Exclude<FrequencyType, 'custom'>
+    switch (type) {
+      case 'hour':
+        return HourlyExprSerializer
+      case 'day':
+        return DailyExprSerializer
+      case 'week':
+        return WeeklyExprSerializer
+      case 'month':
+        return MonthlyExprSerializer
+      case 'year':
+        return YearlyExprSerializer
+      case 'never':
+        return Object
+      default:
+        never(type)
+    }
+  })
+  exprs:
+    | HourlyExprSerializer
+    | DailyExprSerializer
+    | WeeklyExprSerializer
+    | MonthlyExprSerializer
+    | YearlyExprSerializer
 }
 
+// ***************************************************************
+// Custom Repeat Frequency Serializer
+// ***************************************************************
+@Expose()
 export class CustomFrequencySerializer implements CustomFrequency {
-  type: Extract<FrequencyType, 'custom'>
+  @Type(() => String) type: Extract<FrequencyType, 'custom'>
   @Type(() => Date) until: Date | null
   @Type(() => CronScheduleSerializer) crons: CronSchedule[]
 }
 
+// ***************************************************************
+// Task Scheduling Serializer
+// ***************************************************************
+@Expose()
 export class ScheduleSerializer implements TaskSchedule {
   id: string
   taskId: string
