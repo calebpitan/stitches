d49622719cd14d1029978eed1b1e55fadabbd1ff
Caleb Adepitan <calebpitan@gmail.com>
2024-11-05 02:26:41 +0100

    update: task repository, exposing more APIs

--- packages/io/src/repositories/tasks.ts
+++ packages/io/src/repositories/tasks.ts
@@ -1,9 +1,9 @@
-import { eq } from 'drizzle-orm'
+import { and, eq, isNotNull, isNull } from 'drizzle-orm'
 import { SQLJsDatabase } from 'drizzle-orm/sql-js'
 
 import * as schema from '../schema'
 import { fragments } from '../utils'
-import { withUnredacted } from './utils'
+import { withRedacted, withUnredacted } from './utils'
 
 type TaskPayload = typeof schema.tasks.$inferInsert
 type TaskCreatePayload = Omit<TaskPayload, 'updatedAt'>
@@ -47,6 +47,44 @@ export class TasksRepository {
   }
 
   /**
+   * Find as many `Task`s
+   *
+   * @returns A list of `Task`s
+   */
+  findMany() {
+    const result = this.db.select().from(schema.tasks)
+    const tasks = withUnredacted(result, schema.tasks).all()
+
+    return tasks
+  }
+
+  /**
+   * Find a previously redacted `Task` by a given ID
+   *
+   * If the task is not found, return `undefined`
+   *
+   * @returns The **redacted** `Task` matching the provided ID, otherwise `undefined`
+   */
+  findRedactedById(id: string) {
+    const result = this.db.select().from(schema.tasks).where(eq(schema.tasks.id, id))
+    const tasks = withRedacted(result, schema.tasks).all()
+
+    return tasks
+  }
+
+  /**
+   * Find as many previously redacted `Task`s
+   *
+   * @returns A list of previously **redacted** `Task`s
+   */
+  findRedacted() {
+    const result = this.db.select().from(schema.tasks)
+    const tasks = withRedacted(result, schema.tasks).all()
+
+    return tasks
+  }
+
+  /**
    * Update a `Task` by a given ID, applying a patch to it
    *
    * @param id The ID of the `Task` to update
@@ -64,7 +102,7 @@ export class TasksRepository {
 
   /**
    * Redact a `Task` by a given ID, setting the deleted marker for it
-   * to a non-nullable value of `Date`.
+   * to a non-nullable value, if it has not been set.
    *
    * @param id The ID of the `Task` to redact
    * @returns The redacted `Task`
@@ -73,11 +111,29 @@ export class TasksRepository {
     const result = this.db
       .update(schema.tasks)
       .set({ deletedAt: fragments.now })
-      .where(eq(schema.tasks.id, id))
+      .where(and(eq(schema.tasks.id, id), isNull(schema.tasks.deletedAt)))
 
     const redacted = result.returning().get()
 
     return redacted
+  }
+
+  /**
+   * Restore a previously redacted `Task` by a given ID, unsetting the
+   * deleted marker for it with a nullable value, if it has not been unset.
+   *
+   * @param id The ID of the `Task` to restore
+   * @returns The restored `Task`
+   */
+  restore(id: string) {
+    const result = this.db
+      .update(schema.tasks)
+      .set({ deletedAt: null })
+      .where(and(eq(schema.tasks.id, id), isNotNull(schema.tasks.deletedAt)))
+
+    const restored = result.returning().get()
+
+    return restored
   }
 
   /**
