71f2df8db863cc70f69cc4629ac68c36782d48d3
Caleb Adepitan <calebpitan@gmail.com>
2024-11-07 00:24:13 +0100

    feat: add an overflow box for tracking and visually aiding overflown containers

---
+++ src/components/overflow/OverflowBox.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, watch } from 'vue'
+
+import { useScroll } from '@vueuse/core'
+
+type Tracker = 'top' | 'right' | 'bottom' | 'left'
+
+const box = ref<HTMLDivElement | null>(null)
+const tracker = ref<Set<Tracker>>(new Set())
+const child = computed(() => box.value?.children.item(0) as HTMLElement | null)
+
+const scroll = useScroll(child, { behavior: 'smooth' })
+
+onMounted(() => {
+  watch(
+    () => scroll,
+    (value) => {
+      const { arrivedState } = value
+      const target = child.value!
+      const { scrollHeight, scrollWidth, clientHeight, clientWidth } = target
+
+      if (scrollHeight === clientHeight) {
+        tracker.value.delete('top')
+        tracker.value.delete('bottom')
+      }
+
+      if (scrollWidth === clientWidth) {
+        tracker.value.delete('left')
+        tracker.value.delete('right')
+      }
+
+      const entries = Object.entries(arrivedState)
+      const arrived = entries.filter(([, v]) => v).map(([k]) => k as Tracker)
+      const unarrived = entries.filter(([, v]) => !v).map(([k]) => k as Tracker)
+
+      arrived.forEach((v) => tracker.value.delete(v))
+      unarrived.forEach((v) => tracker.value.add(v))
+    },
+    { immediate: true, deep: true }
+  )
+})
+</script>
+
+<template>
+  <div v-if="$slots.default" :class="['s-overflowbox', ...tracker]" ref="box">
+    <slot ref="el" />
+  </div>
+</template>
+
+<style scoped>
+.s-overflowbox {
+  --start-color: var(--s-surface-elevated);
+
+  @media (prefers-color-scheme: dark) {
+    --start-color: var(--s-surface-ground);
+  }
+
+  position: relative;
+
+  &.top {
+    &::before {
+      --gradient: linear-gradient(to bottom, var(--start-color), transparent);
+      top: 0;
+    }
+  }
+
+  &.bottom {
+    &::after {
+      --gradient: linear-gradient(to top, var(--start-color), transparent);
+      bottom: 0;
+    }
+  }
+
+  &.top::before,
+  &.bottom::after {
+    width: 100%;
+    height: 200px;
+  }
+
+  &.top::before,
+  &.bottom::after {
+    content: '';
+    z-index: 1;
+    display: block;
+    position: absolute;
+    background: var(--gradient);
+    pointer-events: none;
+  }
+}
+</style>
