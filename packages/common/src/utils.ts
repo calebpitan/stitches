9123ecef54e0dc6e26b57b2a0931d93311378ca8
Caleb Adepitan <calebpitan@gmail.com>
2024-11-11 22:10:35 +0100

    update: add range generator

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -48,3 +48,31 @@ export const turnoff = (bits: number, index: number) => {
  * @returns The converted string
  */
 export const pascalCase = (s: string) => startCase(camelCase(s)).replace(/ /g, '')
+
+/**
+ * Generate a range of integers from `0` to `stop`
+ * @param stop The number, exclusive, at which the range generator stops
+ */
+export function range(stop: number): Generator<number>
+/**
+ * Generate a range of integers from `start` to `stop` taking `step`
+ * progressively
+ *
+ * @param start The number from which the range generator begins
+ * @param stop The number, exclusive, at which the range generator stops
+ * @param step The arithmetic progression for the range generator
+ */
+export function range(start: number, stop: number, step?: number): Generator<number>
+export function* range(stopOrStart: number, stop?: number, step?: number) {
+  // TODO: make range work with integers only due to inconsistencies with float arithmetic
+  let start = 0
+  ;({ start, stop, step } =
+    stop === undefined
+      ? { start, stop: stopOrStart, step: 1 }
+      : { start: stopOrStart, stop: stop, step: Math.sign(stop - stopOrStart) })
+
+  const order = ((a: number, b: number) => (i: number) => (a < b ? i < b : i > b))(start, stop)
+  for (let i = start; order(i); i += step) {
+    yield i
+  }
+}
