95ad57de9dc2a30a73eb9226a0ec8bae5f91e9e0
Caleb Adepitan <calebpitan@gmail.com>
2024-11-17 11:21:37 +0100

    refactor: rename mixin function

--- packages/io/src/repositories/tags.ts
+++ packages/io/src/repositories/tags.ts
@@ -2,14 +2,14 @@ import { SQLJsDatabase } from 'drizzle-orm/sql-js'
 
 import * as schema from '../schema'
 import { TagsToTaskAssociation } from './associations'
-import { RepositoryFactory } from './factory'
+import { RepositoryAbstractFactory } from './factory'
 
 export type TagsPayload = typeof schema.tags.$inferInsert
 export type TagsCreatePayload = Omit<TagsPayload, 'updatedAt'>
 export type TagsUpdatePayload = Omit<TagsPayload, 'id' | 'createdAt'>
 export type TagsSelectResult = typeof schema.tags.$inferSelect
 
-export class TagsRepository extends RepositoryFactory('tags', { table: schema.tags }) {
+export class TagsRepository extends RepositoryAbstractFactory('tags', { table: schema.tags }) {
   /**
    * An association object for managing tags-to-tasks associations and can be
    * used for retrieving tasks associations on this repository by default
--- packages/io/src/repositories/tasks.ts
+++ packages/io/src/repositories/tasks.ts
@@ -2,13 +2,13 @@ import { SQLJsDatabase } from 'drizzle-orm/sql-js'
 
 import * as schema from '../schema'
 import { TagsToTaskAssociation } from './associations'
-import { RepositoryFactory } from './factory'
+import { RepositoryAbstractFactory } from './factory'
 
 export type TaskPayload = typeof schema.tasks.$inferInsert
 export type TaskCreatePayload = Omit<TaskPayload, 'updatedAt'>
 export type TaskUpdatePayload = Omit<TaskPayload, 'id' | 'createdAt'>
 
-export class TasksRepository extends RepositoryFactory('tasks', { table: schema.tasks }) {
+export class TasksRepository extends RepositoryAbstractFactory('tasks', { table: schema.tasks }) {
   /**
    * An association object for managing tags-to-tasks associations and can be
    * used for retrieving tags associations on this repository by default
