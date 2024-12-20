0ad4037479855a5e42835e793aea31a449765fa3
Caleb Adepitan <calebpitan@gmail.com>
2024-12-20 17:36:59 +0100

    chore(test): add config for testing packages/common

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -2,16 +2,24 @@ import { configDefaults, defineWorkspace } from 'vitest/config'
 
 const workspace = defineWorkspace([
   {
-    extends: './vitest.config.ts'
+    extends: './vitest.config.ts',
   },
   {
     test: {
       name: 'io',
       environment: 'jsdom',
       root: './packages/io',
-      exclude: [...configDefaults.exclude]
-    }
-  }
+      exclude: [...configDefaults.exclude],
+    },
+  },
+  {
+    test: {
+      name: 'common',
+      environment: 'node',
+      root: './packages/common',
+      exclude: [...configDefaults.exclude],
+    },
+  },
 ])
 
 export default workspace
