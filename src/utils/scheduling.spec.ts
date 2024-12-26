bb9f738ab66f7f55c3973da2019b9d7c469b0efd
Caleb Adepitan <calebpitan@gmail.com>
2024-12-26 21:34:01 +0100

    update: abstract schedule generation from frequencies, add tests

--- src/utils/scheduling.spec.ts
+++ src/utils/scheduling.spec.ts
@@ -1,59 +1,58 @@
+import type { TaskSchedule } from '@stitches/common'
+
 import { describe, expect, it } from 'vitest'
 
-import {
-  nextHourlySchedule,
-  nextWeeklySchedule,
-  nextWeeklyScheduleCorrectionFactor
-} from './scheduling'
+import { getUpcomingSchedules } from './scheduling'
+import { type datetime } from './utils'
 
-const WEEK_MILLIS = 604_800_000
-const DAY_MILLIS = 86_400_000
-const timestamp = new Date('2024-10-21T14:19:00.000Z')
+describe('scheduling', () => {
+  describe('#getUpcomingSchedules', () => {
+    const schedule: TaskSchedule<'regular'> = {
+      id: '',
+      taskId: '',
+      frequency: {
+        type: 'month',
+        exprs: { every: 2, subexpr: { type: 'ondays', days: [10, 12, 13] } },
+        until: new Date('2026-10-30T16:45:00.520Z'),
+      },
+      timing: {
+        anchor: new Date('2024-10-20T16:45:00.520Z'),
+        naive: '',
+        tzone: '',
+      },
+    }
 
-describe('#nextHourlySchedule', () => {
-  it('should compute the next hourly datetime', () => {
-    const next = nextHourlySchedule(timestamp, 1)
-    const expected = new Date()
+    it('should generate the next schedule for a regular frequency', () => {
+      const upcoming = getUpcomingSchedules(schedule, {
+        curtime: new Date('2024-12-26T20:19:00.000Z'),
+      })
 
-    expected.setUTCHours(
-      expected.getUTCHours() + (expected.getUTCMinutes() > timestamp.getUTCMinutes() ? 1 : 0),
-      timestamp.getUTCMinutes(),
-      timestamp.getUTCSeconds(),
-      timestamp.getUTCMilliseconds()
-    )
-
-    expect(next.toISOString()).toEqual(expected.toISOString())
+      expect(upcoming?.setZone('utc')?.toISO()).toMatchInlineSnapshot(`"2025-02-10T16:45:00.520Z"`)
+    })
   })
-})
 
-describe('#nextWeeklySchedule', () => {
-  it('should compute the next weekly datetime', () => {
-    {
-      const schedule = { weeks: 1, weekday: 4 }
-      const next = nextWeeklySchedule(timestamp, schedule.weeks)
-      expect(((next.getTime() - timestamp.getTime()) / schedule.weeks) % WEEK_MILLIS).toEqual(0)
-    }
-
-    {
-      const schedule = { weeks: 2, weekday: 4 }
-      const next = nextWeeklySchedule(timestamp, schedule.weeks)
-      expect(((next.getTime() - timestamp.getTime()) / schedule.weeks) % WEEK_MILLIS).toEqual(0)
-    }
-  })
-})
-
-describe('#nextWeeklyScheduleCorrectionFactor', () => {
-  it('should compute the correction factor for adjusting days of weekly schedule', () => {
-    {
-      const schedule = { weeks: 1, weekday: 4 }
-      const next = nextWeeklySchedule(timestamp, schedule.weeks)
-      const correctionFactor = nextWeeklyScheduleCorrectionFactor(next, schedule.weekday)
-      const nextDate = new Date(Math.round(next.getTime() * correctionFactor))
-
-      const remainder = ((nextDate.getTime() - timestamp.getTime()) / schedule.weeks) % WEEK_MILLIS
-      const expectedRemainder = Math.abs(nextDate.getDay() - timestamp.getDay()) * DAY_MILLIS
-
-      expect(remainder).toEqual(expectedRemainder)
-    }
+  describe('#getUpcomingSchedules.iterator', () => {
+    it('should generate a series of schedules as an iterator', () => {
+      const it = getUpcomingSchedules.iterator({
+        id: '',
+        taskId: '',
+        frequency: {
+          type: 'month',
+          exprs: { every: 2, subexpr: { type: 'ondays', days: [10, 12, 13] } },
+          until: new Date('2026-10-30T16:45:00.520Z'),
+        },
+        timing: {
+          anchor: new Date('2024-10-20T16:45:00.520Z'),
+          naive: '',
+          tzone: '',
+        },
+      })
+      let itr: IteratorResult<datetime.DateTime>
+      let i = 0
+      while ((itr = it!.next()) && !itr.done && i < 100) {
+        i++
+        console.log('Value %d %s', i, itr.value)
+      }
+    })
   })
 })
--- src/utils/scheduling.ts
+++ src/utils/scheduling.ts
@@ -1,130 +1,155 @@
-import type { Ordinals } from '@stitches/common'
-import { never } from '@stitches/common'
+import type { ScheduleOptions, TaskSchedule } from '@stitches/common'
+import {
+  evaluate,
+  never,
+  nextDailySchedule,
+  nextHourlySchedule,
+  nextMonthlySchedule,
+  nextWeeklySchedule,
+  nextYearlySchedule,
+} from '@stitches/common'
 
-/**
- * Represent and ordianl value as an unsigned 8-bit integer.
- *
- * @param ordinal The oridnal value to represent as an unsigned 8-bit integer
- * @returns An unsigned 8-bit integer that represents the given ordinal value
- */
-export function ordToU8(ordinal: Ordinals) {
-  switch (ordinal) {
-    case 'first':
-      return 0x00
-    case 'second':
-      return 0x01
-    case 'third':
-      return 0x02
-    case 'fourth':
-      return 0x03
-    case 'fifth':
-      return 0x04
-    case 'last':
-      return 0xff
+import { parseExpression } from 'cron-parser'
+
+import { datetime } from './utils'
+
+export function getUpcomingSchedules(schedule: TaskSchedule, options?: ScheduleOptions) {
+  if (schedule.frequency.type === 'never') {
+    return undefined
+  } else if (schedule.frequency.type === 'custom') {
+    return getUpcomingCustomSchedules(schedule as TaskSchedule<'custom'>, options)
+  }
+  return getUpcomingRegualarSchedules.resolve(schedule as TaskSchedule<'regular'>, options)
+}
+
+getUpcomingSchedules.iterator = (schedule: TaskSchedule) => {
+  if (schedule.frequency.type === 'never') {
+    return undefined
+  } else if (schedule.frequency.type === 'custom') {
+    return getUpcomingCustomSchedules.iterator(schedule as TaskSchedule<'custom'>)
+  }
+  return getUpcomingRegualarSchedules.iterator(schedule as TaskSchedule<'regular'>)
+}
+
+export function getUpcomingCustomSchedules(
+  schedule: TaskSchedule<'custom'>,
+  options?: Pick<ScheduleOptions, 'curtime'>,
+): datetime.DateTime | undefined {
+  const upcomings = schedule.frequency.crons
+    .map((cron) =>
+      parseExpression(cron.expression, {
+        currentDate: options?.curtime?.getTime(),
+        startDate: schedule.timing.anchor,
+        endDate: schedule.frequency.until ?? undefined,
+        tz: schedule.timing.tzone,
+      }),
+    )
+    .map((v) => v.next().getTime())
+  const upcoming = Math.min(...upcomings)
+
+  if (Object.is(Infinity, upcoming)) return undefined
+
+  return datetime(upcoming)
+}
+
+getUpcomingCustomSchedules.iterator = function* (schedule: TaskSchedule<'custom'>) {
+  let cursor = getUpcomingCustomSchedules(schedule)
+
+  while (cursor) {
+    yield cursor
+    cursor = getUpcomingCustomSchedules(schedule, { curtime: cursor.toJSDate() })
+  }
+}
+
+export function getUpcomingRegualarSchedules(
+  schedule: TaskSchedule<'regular'>,
+  options?: ScheduleOptions,
+) {
+  switch (schedule.frequency.type) {
+    case 'hour': {
+      return nextHourlySchedule(schedule.timing.anchor, schedule.frequency.exprs.every, options)
+    }
+    case 'day': {
+      return nextDailySchedule(schedule.timing.anchor, schedule.frequency.exprs.every, options)
+    }
+    case 'week': {
+      return nextWeeklySchedule(
+        schedule.timing.anchor,
+        schedule.frequency.exprs.every,
+        schedule.frequency.exprs.subexpr.weekdays,
+        options,
+      )
+    }
+    case 'month': {
+      if (schedule.frequency.exprs.subexpr.type === 'ondays')
+        return nextMonthlySchedule(
+          schedule.timing.anchor,
+          schedule.frequency.exprs.every,
+          schedule.frequency.exprs.subexpr.days,
+          options,
+        )
+      return nextMonthlySchedule(
+        schedule.timing.anchor,
+        schedule.frequency.exprs.every,
+        schedule.frequency.exprs.subexpr,
+        options,
+      )
+    }
+    case 'year': {
+      const on = schedule.frequency.exprs.subexpr.on
+      const ordinals = on && { ordinal: on.ordinal, weekday: (on.weekday || on.variable)! }
+      return nextYearlySchedule(
+        schedule.timing.anchor,
+        schedule.frequency.exprs.every,
+        schedule.frequency.exprs.subexpr.in.months,
+        ordinals,
+        options,
+      )
+    }
     default:
-      never(ordinal)
+      never(schedule.frequency)
   }
 }
 
-export function nextHourlySchedule(timestamp: Date, hours: number) {
-  const hourMillis = 3_600_000 * hours
-  const currentMillis = Date.now()
-  const timestampMillis = timestamp.getTime()
+getUpcomingRegualarSchedules.resolve = (
+  schedule: TaskSchedule<'regular'>,
+  opts?: ScheduleOptions,
+): datetime.DateTime | undefined => {
+  const freq = schedule.frequency
+  const until_ts = 'until' in freq ? freq.until?.getTime() : undefined
 
-  if (timestampMillis >= currentMillis) return timestamp
+  const curtime = opts?.curtime ? datetime(opts.curtime.getTime()) : datetime()
+  const curtime_ts = curtime.toMillis()
+  const upcomings = getUpcomingRegualarSchedules(schedule, opts)
+  const upcoming_ts = evaluate(() => {
+    if (Array.isArray(upcomings)) {
+      const mapped = upcomings.filter((v) => v !== undefined).map((v) => v!.getTime())
+      const filtered = mapped.filter((v) => v > curtime_ts)
+      const min = Math.min(...filtered)
 
-  const elapsed = currentMillis - timestampMillis
-  const elapsedHoursFactor = elapsed / hourMillis
-  const nextHourFactor = Math.ceil(elapsedHoursFactor)
-  const nextHourMillis = hourMillis * nextHourFactor
-  const nextMillis = timestampMillis + nextHourMillis
+      if (Object.is(Infinity, min)) {
+        return undefined
+      }
+      return min
+    }
+    return upcomings.getTime()
+  })
 
-  return new Date(nextMillis)
-}
-
-export function nextDailySchedule(timestamp: Date, days: number) {
-  const dayMillis = 86_400_000 * days
-  const currentMillis = Date.now()
-  const timestampMillis = timestamp.getTime()
-
-  if (timestampMillis >= currentMillis) return timestamp
-
-  const elapsed = currentMillis - timestampMillis
-  const elapsedDaysFactor = elapsed / dayMillis
-  const nextDayFactor = Math.ceil(elapsedDaysFactor)
-  const nextDayMillis = dayMillis * nextDayFactor
-  const nextMillis = timestampMillis + nextDayMillis
-
-  return new Date(nextMillis)
-}
-
-// function nextDailySchedule(timestamp: Date, days: number) {
-//   const DAY_MS = 86_400_000 * days
-//   const currentMillis = Date.now()
-//   const timestampMillis = timestamp.getTime()
-
-//   if (timestampMillis >= currentMillis) return timestamp
-
-//   const elapsed = currentMillis - timestampMillis
-//   const elapsedDays = elapsed / DAY_MS
-//   const nextDay = Math.sign(days) === -1 ? Math.floor(elapsedDays) : Math.ceil(elapsedDays)
-//   const nextDayMillis = Math.abs(nextDay) * DAY_MS
-//   const nextMillis = timestampMillis + nextDayMillis
-
-//   return new Date(nextMillis)
-// }
-
-export function nextWeeklySchedule(timestamp: Date, weeks: number) {
-  const weekMillis = 604_800_000 * weeks
-  const currentMillis = Date.now()
-  const timestampMillis = timestamp.getTime()
-
-  if (timestampMillis >= currentMillis) return timestamp
-
-  const elapsed = currentMillis - timestampMillis
-  const elapsedWeeksFactor = elapsed / weekMillis
-  const nextWeekFactor = Math.ceil(elapsedWeeksFactor)
-  const nextWeekMillis = weekMillis * nextWeekFactor
-  const nextMillis = timestampMillis + nextWeekMillis
-
-  return new Date(nextMillis)
-}
-
-export function nextWeeklyScheduleCorrectionFactor(nextWeeklyTimestamp: Date, weekday: number) {
-  // `nextWeeklySchedule` add `x` weeks to the originating timestamp.
-  //
-  // `let x = 1` [1wk; 604800000ms] such that:
-  //
-  // on Thu 24, Oct 2024, `nextWeeklySchedule("2024-10-24")` returns `"2024-10-31"`
-  // which is a Thu 31, Oct 2024.
-  //
-  // But the user wants the event repeating every 1 week on a Tuesday (weekday = 2);
-  // So we subtract Tuesday (weekday = 2) from Thursday (weekday = 4) to compute our
-  // `weekdayOffset`.
-  //
-  // We go ahead and clone `nextWeeklyTimestamp`, setting date to be 31 + 2 * -1
-  // which gives `29` and that's a Tuesday one week away from the past week of 24th
-  const weekdayOffset = nextWeeklyTimestamp.getUTCDay() - weekday
-  const newWeeklyTimestamp = new Date(nextWeeklyTimestamp.getTime())
-
-  newWeeklyTimestamp.setDate(newWeeklyTimestamp.getDate() + weekdayOffset * -1)
-
-  return newWeeklyTimestamp.getTime() / nextWeeklyTimestamp.getTime()
-}
-
-export function month(every: number, start: { month: number; year: number }) {
-  const MAX_MONTH = 12
-  const date = new Date()
-  const end = { month: date.getMonth(), year: date.getFullYear() }
-
-  if (end.year < start.year) {
-    // The inital schedule is still ahead of now
-    return
+  if (until_ts && upcoming_ts && upcoming_ts > until_ts) return undefined
+  if (upcoming_ts === undefined || upcoming_ts <= curtime_ts) {
+    return getUpcomingRegualarSchedules.resolve(schedule, {
+      ...opts,
+      curtime: curtime.plus({ [freq.type]: 1 }).toJSDate(),
+    })
   }
 
-  const monthDifference = end.month - start.month
-  const yearDifference = end.year - start.year
-  const monthsElapsed = monthDifference + MAX_MONTH * yearDifference
+  return datetime(upcoming_ts)
+}
 
-  return (every - (monthsElapsed % every)) % every
+getUpcomingRegualarSchedules.iterator = function* (schedule: TaskSchedule<'regular'>) {
+  let cursor = getUpcomingRegualarSchedules.resolve(schedule)
+  while (cursor) {
+    yield cursor
+    cursor = getUpcomingRegualarSchedules.resolve(schedule, { curtime: cursor.toJSDate() })
+  }
 }
